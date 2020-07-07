14ef82e59bb951956bd82286d79234000706e778
rockebellml <rockebellml@gmail.com>
2020-07-07 15:16:42 -0300

    pasta de html, css, js e node

---
+++ js/scriptRedesSocial.js
@@ -0,0 +1,29 @@
+/*function redeSocial(idRedeSocial){
+    if(idRedeSocial === 'facebook'){
+        window.location.assign('https://m.facebook.com/bmchrisostomo')
+    }
+    else if(idRedeSocial === 'github'){
+        window.location.assign('https://rockebellml.github.io/brunochrisostomo.github.io/')
+    }
+    else if(idRedeSocial === 'linkedin'){
+        window.location.assign('https://www.linkedin.com/in/bruno-chrisostomo-305b3512b')
+    }
+    else if(idRedeSocial === 'whatsapp'){
+        window.location.assign(' https://wa.me/5519974027991/?text=urlencodedtext')
+    }
+    else{
+        alert('ERROR')
+    }
+}*/
+function facebook(){
+    window.location.href = "https://m.facebook.com/bmchrisostomo"
+}
+function github(){
+    window.location.href = "www.github.com"
+}
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/bruno-chrisostomo-305b3512b"
+}   
+function whatsapp(){
+    window.location.href = "https://wa.me/5519974027991/?text=urlencodedtext"
+}
